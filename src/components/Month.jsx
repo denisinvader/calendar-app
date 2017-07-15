340876377cef6f7cf4273efaf1e3f7522370c932
Mikhail Panichev <denis.invader@gmail.com>
2017-07-15 12:41:44 +0700

    Month component fixes

--- src/components/Month.jsx
+++ src/components/Month.jsx
@@ -10,26 +10,36 @@ function Calendar ({ month, year, locale }) {
   d.date(1);
 
   var weekDays = [];
+  var calendar = {};
   for (let i = 0; i < 7; i++) {
     d.weekday(i);
+
+    if (d.month() != month) {
+      if (!calendar[d.week()])
+        calendar[d.week()] = [];
+
+      calendar[d.week()].push(<td key={`_d_${d.date()}`}></td>);
+    }
 
     weekDays.push(
       <td key={`wd_${i}`}>{ d.format('ddd') }</td>
     );
   }
 
-  d.weekday(0);
-  var calendar = {};
-  while (d.month() <= month) {
+  d.date(1);
+  while (d.month() == month) {
     if (!calendar[d.week()])
       calendar[d.week()] = [];
 
     if (d.month() === month)
       calendar[d.week()].push(<td key={`d_${d.date()}`}>{ d.date() }</td>);
-    else
-      calendar[d.week()].push(<td key={`_d_${d.date()}`}></td>);
 
     d.date(d.date() + 1);
+  }
+
+  if (calendar.hasOwnProperty(1) && !calendar.hasOwnProperty(2)) {
+    calendar[Math.max.apply(null, Object.keys(calendar)) + 1] = calendar[1];
+    delete calendar[1];
   }
 
   var weeks = [];
